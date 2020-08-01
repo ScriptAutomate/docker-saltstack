7fddfc0a8a9b0b11d5e2eaa713720551a9be793d
ScriptAutomate <derek@icanteven.io>
2020-08-01 03:21:23 -0500

    Extending debian targets; starting Windows attempts

---
+++ extras/windows-machine.sh
@@ -0,0 +1,10 @@
+# Repo of reference
+## https://github.com/StefanScherer/windows-docker-machine
+## For experimenting with Windows docker machines
+## Attempting to work with Windows container minions?
+
+# ~20GB download?
+vagrant box add --provider virtualbox StefanScherer/windows_2019_docker
+
+# ~80 - 150GB download?
+vagrant box add --provider virtualbox StefanScherer/windows_2019
